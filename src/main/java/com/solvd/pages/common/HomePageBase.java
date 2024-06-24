386f46477d263a62a3b4274a156bf61686036d62
whetman <whetman@solvd.com>
2024-06-24 12:33:30 +0200

    created base for home page and home page, created basic test to see if the application is opening

---
+++ src/main/java/com/solvd/pages/common/HomePageBase.java
@@ -0,0 +1,15 @@
+package com.solvd.pages.common;
+
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HomePageBase extends AbstractPage {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomePageBase.class);
+
+    public HomePageBase(WebDriver driver) {
+        super(driver);
+    }
+}
