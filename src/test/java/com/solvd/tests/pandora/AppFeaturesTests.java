80fb39b85fe19b90cc65d983a284a2b5de077e0a
whetman <whetman@solvd.com>
2024-07-10 15:37:57 +0200

    created AddressBook

--- src/test/java/com/solvd/tests/pandora/AppFeaturesTests.java
+++ src/test/java/com/solvd/tests/pandora/AppFeaturesTests.java
@@ -1,5 +1,6 @@
 package com.solvd.tests.pandora;
 
+import com.solvd.pandora.pages.common.AddressBookBase;
 import com.solvd.pandora.pages.common.HomePageBase;
 import com.solvd.pandora.pages.common.MyAccountPageBase;
 import com.solvd.pandora.pages.common.WelcomePageBase;
@@ -13,16 +14,20 @@ public class AppFeaturesTests extends AbstractTest {
 
     //in progress!
     @Test(testName = "#TC0001")
-    public void validateAddressBook() {
+    public void validateAddressBook() throws InterruptedException {
         SoftAssert sa = new SoftAssert();
         HomePageBase homePage = initPage(getDriver(), HomePageBase.class);
 
         homePage.isPageOpened();
 
-        MyAccountPageBase myAccountPageBase = homePage.getHeader().clickLoginButton();
-        WelcomePageBase welcomePage = myAccountPageBase.login(R.TESTDATA.get("email"), R.TESTDATA.get("password"));
+        MyAccountPageBase myAccountPage = homePage.getHeader().clickLoginButton();
+        WelcomePageBase welcomePage = myAccountPage.login(R.TESTDATA.get("email"), R.TESTDATA.get("password"));
 
         welcomePage.isPageOpened();
+
+        AddressBookBase addressBook = welcomePage.getHeader().openAddressBook();
+        addressBook.isPageOpened();
+
 
     }
 }
